9aee4df05ac380689d383f90ef99599fadea6fa6
Milo <50248166+Milo123459@users.noreply.github.com>
2022-06-09 22:47:31 +0100

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #[tokio::main]
 async fn main() {
     for _ in 1..1001 {
-        tokio::task::spawn_blocking(|| async move { fun().await });
+        tokio::task::spawn_blocking(|| async move { fun().await }).await;
     }
 }
 
