334cd719caf0d97ac6ca8f2a541f88e61a635e9c
Milo <50248166+Milo123459@users.noreply.github.com>
2022-06-09 20:21:53 +0100

    rusty break railway

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+#[tokio::main]
+async fn main() {
+    for _ in 1..1001 {
+        tokio::task::spawn_blocking(|| async move { fun().await });
+    }
+}
+
+async fn fun() {
+    println!("rusty 😲")
+}
